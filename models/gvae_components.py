2e55361c300707b31b13bc3f56f6017fc69c70ac
Lê Kỳ Anh <anh.lk207586@sis.hust.edu.vn>
2025-11-08 14:32:26 +0700

    Update Attribute Decoder

--- models/gvae_components.py
+++ models/gvae_components.py
@@ -150,7 +150,7 @@ class AttributeDecoder(nn.Module):
                  original_feature_dim: int, 
                  hidden_dim_multiplier: int = 4,
                  num_residual_blocks: int = 2,   
-                 dropout: float = 0.15):         
+                 dropout: float = 0.2):         
         """
         Args:
             latent_dim: Dimensionality of the latent embeddings.
